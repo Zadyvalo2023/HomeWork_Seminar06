f9899a9cd9203caf348f2ad26e986da8243947e1
Zadyvalo2023 <av1469400@gmail.com>
2023-06-29 00:09:16 +0300

    My work

---
+++ Task041/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+class Program 
+{
+    static void Main() 
+    {
+        Console.Write("Введите количество чисел:");
+
+        int M = int.Parse(Console.ReadLine());
+
+        int countPositiveNumbers = 0;
+
+        for (int i = 0; i < M; i++)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                countPositiveNumbers++;
+            }
+        }
+        Console.WriteLine("Количество чисел больше 0: " + countPositiveNumbers);
+    }
+}
---
+++ Task043/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+class Program{
+    static void Main()
+    {
+        double k1, b1, k2, b2;
+        Console.WriteLine("ВВедите значение k1:");
+        k1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("ВВедите значение b1:");
+        b1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("ВВедите значение k2:");
+        k2= double.Parse(Console.ReadLine());
+
+        Console.WriteLine("ВВедите значение b2:");
+        b2 = double.Parse(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения: ({x}, {y})");
+        Console.ReadLine();
+
+    }
+}
